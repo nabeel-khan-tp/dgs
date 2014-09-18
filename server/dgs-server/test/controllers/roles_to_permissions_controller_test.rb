e478b9e9aeba4d034f393668091db26c0536a9d4
Daniyal Rao <raothegreat_dani_91@hotmail.com>
2014-09-18 16:34:10 +0500

    updates

---
+++ server/dgs-server/test/controllers/roles_to_permissions_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RolesToPermissionsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ server/dgs-server/test/helpers/roles_to_permissions_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class RolesToPermissionsHelperTest < ActionView::TestCase
+end
